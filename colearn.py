cfe0ead24b175d296a27a3ac8518929cc1483998
deby <doukhansahin@gmail.com>
2020-08-13 04:25:35 -0400

    darknet update

--- colearn.py
+++ colearn.py
@@ -3,8 +3,12 @@ import ctypes
 # net = darknet.load_net(r'cfg/anprtiny.cfg', r'weights/anprtiny_final.weights', 0)
 # meta = darknet.load_meta(r"test.data")
 # r = detect(net, meta, "data/IMG_20200729_134303.jpg")
-meta = darknet.load_meta(ctypes.c_char_p(b'test.data'))
-net = darknet.load_net(b'cfg/anprspp.cfg', b'weights/anprspp_final.weights', 0)
+# meta = darknet.load_meta(ctypes.c_char_p(b'test.data'))
+# net = darknet.load_net(b'cfg/anprspp.cfg', b'weights/anprspp_final.weights', 0)
 
-r = darknet.detect(net, meta, b'data/IMG_20200729_134303.jpg')
+# r = darknet.detect(net, meta, b'data/IMG_20200729_134401.jpg')
+
+network, class_names, colors = darknet.load_network('cfg/anprspp.cfg','test.data','weights/anprspp_final.weights')
+darknet.load_image()
+r = darknet.detect_image(network,class_names,"data/IMG_20200729_134303.jpg")
 print(r)
